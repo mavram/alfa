a6c31d2953203c27264b8c98b650b457cb80f3af
mircea <mavram@gmail.com>
2024-11-15 23:48:58 -0500

    clean-up

--- alfa/sql/drop_tables.sql
+++ alfa/sql/drop_tables.sql
@@ -1,9 +1,12 @@
+-- Turn off foreign key checks to avoid issues with dependent tables
 PRAGMA foreign_keys = OFF;
 
 DROP TABLE IF EXISTS position;
 DROP TABLE IF EXISTS portfolio;
-DROP TABLE IF EXISTS watchlist;
+DROP TABLE IF EXISTS stock_to_watch;
 DROP TABLE IF EXISTS price;
 DROP TABLE IF EXISTS stock;
+DROP TABLE IF EXISTS txn;
 
+-- Turn foreign key checks back on
 PRAGMA foreign_keys = ON;

ec8b9290db85668e4d2d5bd034af3100d6cde8ba
mircea <mavram@gmail.com>
2024-11-26 22:44:54 -0500

    pytest args

--- src/alfa/__main__.py
+++ src/alfa/__main__.py
@@ -14,7 +14,5 @@ if __name__ == "__main__":
     # p.deposit_stock("TSLA", 1000, 120)
     # p.withdraw(100000)
 
-    # pytest --cov=alfa --cov-report=term-missing
-
     print(settings.as_dict())
     pass
